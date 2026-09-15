b5d8216d101cd4f2f131b79be8a9d1f2c7bfa86b
ArYan Dubey <dubeyaryan796@gmail.com>
2026-09-15 11:10:49 +0530

    Correct eslint config imports and plugin names
    Fix import statement and update plugin references.

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import js from '@eslint/js';
+import js from '@eslint/js ';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
@@ -14,8 +14,8 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-hooks': reactedHooks,
+      'react-refresh': reactedRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
